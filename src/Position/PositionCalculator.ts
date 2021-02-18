c9cc260f1398148cc3cd0f5e0cb180f85da55630
DaanV2 <daanverstraten@hotmail.com>
2021-02-18 16:32:41 +0100

    Updating

--- src/Position/PositionCalculator.ts
+++ src/Position/PositionCalculator.ts
@@ -53,8 +53,8 @@ export namespace PositionCalculator {
    * @param FirstLineOffset The text offset of the first line. Usually indicates where the text starts from in the line itself
    * @param LineOffset The line offset of the text. Incase text from line 3 to the end has been grabbed.
    */
-  export function Create(text: string, FirstLineOffset: number = 0, LineOffset: number = 0): PositionCalculator {
-    return new TextCalculator(text, FirstLineOffset, LineOffset);
+  export function Create(text: string): PositionCalculator {
+    return new DocumentCalculator(TextDocument.create("converison", "text", 0, text));
   }
 }
 
@@ -94,75 +94,5 @@ class DocumentCalculator implements PositionCalculator {
    */
   offsetAt(pos: Position): number {
     return this.doc.offsetAt(pos);
-  }
-}
-
-/**
- *The class that wraps around texts and into a PositionCalculator
- */
-class TextCalculator implements PositionCalculator {
-  private FirstLineOffset: number;
-  private LineOffset: number;
-  private LineOffsets: number[];
-
-  constructor(text: string, FirstLineOffset: number, LineOffset: number) {
-    this.FirstLineOffset = FirstLineOffset;
-    this.LineOffset = LineOffset;
-
-    //records at what offset the line has ended
-    this.LineOffsets = CalculateLineOffset(text, FirstLineOffset);
-  }
-
-  /**
-   * Calculates the given position of the given offset
-   * @param offset The offset of the item in the text;
-   */
-  positionAt(offset: number): Position {
-    let LineIndex = 0;
-    let LO = 0;
-
-    //Find in which line this offset is.
-    for (LineIndex = 0; LineIndex < this.LineOffsets.length; LineIndex++) {
-      LO = this.LineOffsets[LineIndex];
-
-      //If the Offset of the line is bigger then the offset then line number has been found
-      if (LO > offset) {
-        //Grab the start of the line
-        LO = this.LineOffsets[Math.max(LineIndex - 1, 0)];
-        break;
-      }
-    }
-
-    //Create output
-    let Out: Position = {
-      //Character in the line is the lineoffset - current
-      character: LO - offset,
-      line: LineIndex + this.LineOffset,
-    };
-
-    //If its the first line it might be offsetted
-    if (LineIndex == 0) {
-      Out.character += this.FirstLineOffset;
-    }
-
-    return Out;
-  }
-
-  rangeOf(startindex: number, endindex: number): Range {
-    return {
-      start: this.positionAt(startindex),
-      end: this.positionAt(endindex),
-    };
-  }
-
-  /**
-   * Calculates the given offset of a position
-   * @param pos The position to retrieve of number of
-   */
-  offsetAt(pos: Position): number {
-    if (pos.line <= 0) return pos.character;
-
-    var Offset = this.LineOffsets[pos.line - 1];
-    return Offset + pos.character;
   }
 }
